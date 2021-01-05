28dcab97d54a3f44f7b092219dd567dd68475522
JanoschV <julianvenczel@gmail.com>
2021-01-05 09:44:30 +0000

    conditionals added

---
+++ Booleans.js
@@ -0,0 +1,40 @@
+// console.log(true,false,'true','false');
+// let names = ['snap','crackle','pop'];
+
+// let email = 'jules_venczel@hotmail.com';
+
+// let result  = email.includes('!');
+// result = names.includes('crunch');
+// console.log(result);
+
+let age = 25;
+
+// console.log(age==25);
+// console.log(age==30);
+// console.log(age!=30);
+// console.log(age>20);
+// console.log(age<20);
+
+// let name = 'steve';
+
+// console.log(name =='steve');
+
+// console.log(name =='crystal');
+
+// console.log(name >'Steve');
+
+// console.log(name>'Crystal');
+
+console.log(age === 25);
+
+console.log(age === '25');
+
+console.log(age !== 25);
+
+console.log(age !== '25');
+
+// let score = '100';
+
+// score = Number(score);
+
+// console.log(score +1);
---
+++ conversion.js
@@ -0,0 +1,17 @@
+let score = '100';
+
+score = Number(score);
+
+console.log(score + 1);
+
+console.log(typeof score);
+
+//let result = Number('hello');
+
+//let result = Number('50');
+
+//let result = Boolean(010);
+
+let result = Boolean('0');
+
+console.log(result,typeof(result));
